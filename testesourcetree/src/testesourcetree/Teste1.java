b940d12c9f87bd9d63c28d5c92d47469e7a257dd
Rafael Ohoseki <ohoseki@hotmail.com>
2019-08-24 11:12:57 -0300

    teste sourcetree

---
+++ testesourcetree/src/testesourcetree/Teste1.java
@@ -0,0 +1,12 @@
+package testesourcetree;
+
+public class Teste1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Meu Deus do ceu");
+
+	}
+
+}
